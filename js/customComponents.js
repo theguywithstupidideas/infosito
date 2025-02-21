f7cc54b450f03cdd84505b71046977ba15a2755a
Davide Prestileo <davidepresti.leo252@gmail.com>
2025-02-21 10:04:06 +0100

    fixes

--- js/customComponents.js
+++ js/customComponents.js
@@ -1,9 +1,13 @@
 class MyComponent extends HTMLElement {
     constructor() {
         super();
-        const shadow = this.attachShadow({mode: 'open'});
-        shadow.innerHTML = `
-            <h1>Hello, world!</h1>
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback() {
+        const test = this.getAttribute('data-test');
+        this.shadowRoot.innerHTML = `
+            <h1>Hello, world! ${test}</h1>
             <slot></slot>
         `;
     }
